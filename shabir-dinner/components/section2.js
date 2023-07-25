82aeee4fbcf608348de52db0eb3b854b5d811916
erickadikah <erickadikah2030@gmail.com>
2023-07-26 01:52:30 +0300

    ui change  and update smart relay

--- shabir-dinner/components/section2.js
+++ shabir-dinner/components/section2.js
@@ -1,12 +1,22 @@
 import React from "react";
+import Image from "next/image";
+import photo from "../public/images/chef.svg";
+import { GiForkKnifeSpoon } from "react-icons/gi";
 
 const section2 = () => {
   return (
     <section
-      style={{ width: "100vw", height: '100%',background: "#E9F8F5" }}
-      className=""
+      style={{ width: "100vw", height: "100%", background: "#E9F8F5" }}
+      className=" items-center mt-20 sm:block px-10"
     >
-      <h1 className="text-center py-10 font-extrabold text-3xl"> Our Top Categories</h1>
+      <div className="flex justify-center align-center py-20">
+        <GiForkKnifeSpoon size={40}/>
+        {/*<Image src={photo} className="w-20 h-20" />*/}
+      </div>
+      <h1 className="text-center font-extrabold text-3xl border rounded-full">
+        {" "}
+        Our Top Categories
+      </h1>
       <div className="flex-1/4 sm:flex-1/4 flex flex-wrap justify-center gap-4 mt-20 py-20">
         <div className="rounded-full w-40 h-10 bg-customYellow flex justify-center items-center hover:bg-yellow-500">
           Chicken
--- shabir-dinner/components/section3.js
+++ shabir-dinner/components/section3.js
@@ -10,8 +10,7 @@ const section3 = () => {
   return (
     <section
       style={{ width: "100vw", height: "100%", background: "#FFFFFF" }}
-      className="justify-center items-center lg:flex sm:block px-10"
-    >
+      className="justify-center items-center sm:block px-10">
       <div class="block md:flex mt-20">
         <div className="">
           <Image src="/images/grill1.jpg " width={400} height={200} />
@@ -26,25 +25,23 @@ const section3 = () => {
             </span>
           </h1>
           <p className="block mt-8 break-words">
-            Lorem ipsum dolor sit amet consectetur <br></br>adipisicing elit. Quisquam
-            voluptatum, voluptate, quibusdam, <br></br> quia voluptas quod quos dolorum
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptatum, voluptate, quibusdam, quia voluptas quod quos dolorum
             voluptatibus quae quas fugit. <br></br>Quisquam voluptatum, voluptate,
           </p>
           <br></br>
           <p>
             Lorem ipsum dolor sit amet consectetur<br></br> adipisicing elit. Quisquam
             voluptatum, voluptate, quibusdam, quia<br></br> voluptas quod quos dolorum
-            voluptatibus quae quas fugit. Quisquam<br></br> voluptatum, voluptate,
-            quibusdam,
           </p>
           <br></br>
           <ul class="flex gap-10 sm:flex-row">
             <ul class="list-none custom-list text-gray-600">
               <li> Order </li>
-              <li> Find variety of food</li>
+              <li> variety of food</li>
               <li> Cook With Me</li>
               <li> Order </li>
-              <li> Find variety of food</li>
+              <li> Find food</li>
             </ul>
             <ul class="list-none custom-list text-gray-600">
               <li> Order </li>
@@ -68,11 +65,62 @@ const section3 = () => {
           </div>
         </div>
       </div>
-      {/*<div className="flex justify-center">
-      <div className=" bg-slate-400 shadow-md hover:bg-inherit">
-        <Image src="/images/grill1.jpg" width={200} height={200} />
+      <div class="block md:flex mt-20">
+      <div class="block md:flex mt-20">
+        <div className="">
+          <Image src="/images/pilau.jpg " width={400} height={200} />
+        </div>
+        <div className="flex-1 px-6">
+          <h1 className="text-xl font-bold">Get Variety Of Articles On Cooking </h1>
+          <h1 className="text-3xl font-extrabold">
+            Welcome to Dinner{" "}
+            <span className="text-yellow-500">
+              the best place to find recipes for
+              and order food{" "}
+            </span>
+          </h1>
+          <p className="block mt-8 break-words">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptatum, voluptate, quibusdam, quia voluptas quod quos dolorum
+            voluptatibus quae quas fugit. <br></br>Quisquam voluptatum, voluptate,
+          </p>
+          <br></br>
+          <p>
+            Lorem ipsum dolor sit amet consectetur<br></br> adipisicing elit. Quisquam
+            voluptatum, voluptate, quibusdam, quia<br></br> voluptas quod quos dolorum
+          </p>
+          <br></br>
+          <ul class="flex gap-10 sm:flex-row">
+            <ul class="list-none custom-list text-gray-600">
+              <li> Order </li>
+              <li> variety of food</li>
+              <li> Cook With Me</li>
+              <li> Order </li>
+              <li> Find food</li>
+            </ul>
+            <ul class="list-none custom-list text-gray-600">
+              <li> Order </li>
+              <li> Find variety of food</li>
+              <li> Cook With Me</li>
+              <li> Order </li>
+              <li> Find variety of food</li>
+            </ul>
+          </ul>
+          <br></br>
+          <div
+            className=" bg-yellow-500 flex justify-center items-center mb-20 hover:bg-yellow-700"
+            style={{
+              width: "150px",
+              height: "60px",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <p className=" text-sm text-white">Read My Articles</p>
+          </div>
+        </div>
       </div>
-        </div>*/}
+      </div>
     </section>
   );
 };
