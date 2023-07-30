08c2948d22dc1477eaf7d171f51e98f884b4c402
erickadikah <erickadikah2030@gmail.com>
2023-07-30 22:24:16 +0300

    render Image grid

--- shabir-dinner/components/section2.js
+++ shabir-dinner/components/section2.js
@@ -11,28 +11,28 @@ const section2 = () => {
       style={{ width: "100vw", height: "100%", background: "#E9F8F5" }}
       className=" items-center mt-20 sm:block px-10"
     >
-    <div className="flex flex-wrap justify-center items-center gap-2">
-    <div className="flex justify-center  py-8">
-    <div className="rounded-full shadow-lg p-8">
-      <GiForkKnifeSpoon size={35} />
-    </div>
-  </div>
-  <div className="flex justify-center  py-8">
-    <div className="rounded-full shadow-lg p-8">
-      <IoFastFoodSharp size={35} />
-    </div>
-  </div>
-  <div className="flex justify-center  py-8">
-    <div className="rounded-full shadow-lg p-8">
-      <PiHamburgerBold size={35} />
-    </div>
-  </div>
-    </div>
+      <div className="flex flex-wrap justify-center items-center gap-2">
+        <div className="flex justify-center  py-8">
+          <div className="rounded-full shadow-lg p-8">
+            <GiForkKnifeSpoon size={35} />
+          </div>
+        </div>
+        <div className="flex justify-center  py-8">
+          <div className="rounded-full shadow-lg p-8">
+            <IoFastFoodSharp size={35} />
+          </div>
+        </div>
+        <div className="flex justify-center  py-8">
+          <div className="rounded-full shadow-lg p-8">
+            <PiHamburgerBold size={35} />
+          </div>
+        </div>
+      </div>
       <h1 className="text-center font-extrabold text-3xl">
         {" "}
         Our Top Categories
       </h1>
-      <div className="flex-1/4 sm:flex-1/4 flex flex-wrap justify-center gap-4 mt-20 py-20">
+      <div className="flex-1/4 md:flex-1/4 flex flex-wrap justify-center gap-4 mt-20 py-20">
         <div className="rounded-full w-40 h-10 bg-customYellow flex justify-center items-center hover:bg-yellow-500">
           Chicken
         </div>
@@ -64,6 +64,38 @@ const section2 = () => {
           <p>Grill</p>
         </div>
       </div>
+     {/* <div className="image-container mb-10 gap-3 flex md:flex-1/2 justify-center items-center flex-wrap">
+        <Image
+          src="/images/grill2.jpg"
+          className="rounded-md"
+          width={200}
+          height={100}
+        />
+        <Image
+          src="/images/grill2.jpg"
+          className="rounded-md"
+          width={200}
+          height={100}
+        />
+        <Image
+          src="/images/grill2.jpg"
+          className="rounded-md"
+          width={200}
+          height={100}
+        />
+        <Image
+          src="/images/grill2.jpg"
+          className="rounded-md"
+          width={200}
+          height={100}
+        />
+  </div>*/}
+      {/*<div class="grid flex-wrap grid-cols-4 gap-4">
+  <div class="bg-blue-300">Column 1</div>
+  <div class="bg-green-300">Column 2</div>
+  <div class="bg-red-300">Column 3</div>
+  <div class="bg-yellow-300">Column 4</div>
+  </div>*/}
     </section>
   );
 };
--- shabir-dinner/components/section3.js
+++ shabir-dinner/components/section3.js
@@ -11,85 +11,97 @@ const section3 = () => {
     <section
       style={{ width: "100vw", height: "100%", background: "#FFFFFF" }}
       className="justify-center items-center sm:block px-10">
-      <div class="block md:flex mt-20 justify-center items-center">
-        <div className="">
-          <Image src="/images/grill1.jpg " width={400} height={200} />
-        </div>
-        <div className="flex-1 px-4 justify-center items-center">
-          <h1 className="text-xl font-bold"> ABOUT US</h1>
-          <h1 className="text-2xl font-extrabold">
+      <div class="block md:flex mt-20 justify-center items-center rounded-xl bg-gray-700">
+        <div className="flex-1 px-8 justify-center items-center border">
+         {/* <div className=" flex flex-wrap gap-4 mt-5 justify-center items-center">
+            <Image
+              src="/images/test.jpg "
+            className="rounded-md"
+              width={200}
+              height={100}
+            />
+            <Image
+              src="/images/grill2.jpg "
+              className="rounded-md"
+              width={200}
+              height={100}
+            />
+                        <Image
+              src="/images/grill2.jpg "
+              className="rounded-md"
+              width={200}
+              height={100}
+            />
+                        <Image
+              src="/images/grill2.jpg "
+              className="rounded-md"
+              width={200}
+              height={100}
+            />
+                        <Image
+              src="/images/grill2.jpg "
+              className="rounded-md"
+              width={200}
+              height={100}
+            />
+              <Image
+              src="/images/grill2.jpg "
+              className="rounded-md"
+              width={200}
+              height={100}
+            />
+  </div>*/}
+          <h1 className="text-xl font-bold text-center mt-4"> ABOUT US</h1>
+          <h1 className="text-2xl font-extrabold text-center">
             Welcome to Dinner{" "}
             <span className="text-yellow-500">
-              the best place to find recipes for
-              and order food{" "}
+              the best place to find recipes for and order food{" "}
             </span>
           </h1>
-          <p className="block mt-2 break-words">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-            voluptatum, voluptate, quibusdam, quia voluptas quod quos dolorum
-            voluptatibus quae quas fugit.  voluptatum, voluptate,
+          <p className="block mt-2 break-words text-center text-white">
+          At TasteBuds Bistro, we 
+          believe that food is an art that should ignite your taste buds 
+          and leave lasting memories. 
+          Our culinary journey began in 2005 when our founder, Chef Emily, 
+          turned her passion for crafting exquisite dishes into a reality. With a vision to create a haven for food lovers, TasteBuds Bistro was born.
+
           </p>
           <br></br>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-            voluptatum, voluptate, quibusdam, quia voluptas quod quos dolorum
+          <p className="text-center text-white">
+            Experience a fusion of flavors that celebrate both local 
+            traditions and global influences. Our carefully curated menu showcases a diverse range of dishes, 
+            from classic favorites to innovative creations.
           </p>
           <br></br>
-          <ul class="flex gap-8 sm:flex-row">
-            <ul class="list-none custom-list text-gray-600">
-              <li> Order </li>
-              <li> variety of food</li>
-              <li> Cook With Me</li>
-              <li> Order </li>
-              <li> Find food</li>
-            </ul>
-            <ul class="list-none custom-list text-gray-600">
-              <li> Order </li>
-              <li> Find variety of food</li>
-              <li> Cook With Me</li>
-              <li> Order </li>
-              <li> Find variety of food</li>
-            </ul>
-          </ul>
           <br></br>
-          <div
-            className=" bg-yellow-500 flex justify-center items-center border rounded-lg mb-20 hover:bg-yellow-700"
-            style={{
-              width: "150px",
-              height: "60px",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <p className=" text-lg text-white">Read More</p>
+          <div className="flex justify-center items-center">
+            <div className="bg-yellow-500 flex justify-center items-center border rounded-lg mb-20 hover:bg-yellow-700 w-40 h-10">
+              <p className="text-lg text-white">Order Now</p>
+            </div>
           </div>
         </div>
       </div>
       <div class="block md:flex mt-20 justify-center items-center">
-        <div className="">
-          <Image src="/images/pilau.jpg" width={400} height={400} />
-        </div>
-        <div className="flex-1 px-6">
-          <h1 className="text-3xl font-extrabold">
+        <div class="flex-1 px-6 border">
+          <h1 class="text-3xl font-extrabold text-center mt-8">
             Welcome to Dinner{" "}
-            <span className="text-yellow-500">
-              the best place to find recipes for
-              and order food{" "}
+            <span class="text-yellow-500">
+              the best place to find recipes for and order food{" "}
             </span>
           </h1>
-          <p className="block mt-2 break-words">
+          <p class="block mt-2 break-words text-center">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
             voluptatum, voluptate, quibusdam, quia voluptas quod quos dolorum
             voluptatibus quae quas fugit. Quisquam voluptatum, voluptate,
           </p>
-          <br></br>
-          <p>
+          <br />
+          <p className="text-center">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
             voluptatum, voluptate, quibusdam, quia voluptas quod quos dolorum
           </p>
-          <br></br>
-          <ul class="flex gap-8 sm:flex-row">
-            <ul class="list-none custom-list text-gray-600">
+          <br />
+          <ul class="flex gap-8 sm:flex-row text-center justify-center items-center">
+            <ul class="list-none custom-list text-gray-600 text-center">
               <li> Order </li>
               <li> variety of food</li>
               <li> Cook With Me</li>
@@ -104,19 +116,16 @@ const section3 = () => {
               <li> Find variety of food</li>
             </ul>
           </ul>
-          <br></br>
-          <div
-            className=" bg-yellow-500 flex justify-center items-center border rounded-lg mb-20 hover:bg-yellow-700"
-            style={{
-              width: "150px",
-              height: "60px",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <p className="text-sm text-white">Read My Articles</p>
+          <br />
+         <div className="flex justify-center items-center">
+            <div className="bg-yellow-500 flex justify-center items-center border rounded-lg mb-20 hover:bg-yellow-700 w-40 h-10">
+              <p className="text-lg text-white">Read More</p>
+            </div>
           </div>
         </div>
+        {/*<div class="flex-none px-6">
+          <Image src="/images/pilau.jpg" width={400} height={400} />
+          </div>*/}
       </div>
     </section>
   );
