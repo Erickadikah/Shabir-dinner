851d2e5e9efdf92348782f81ca4a87a3e6219ba8
erickadikah <erickadikah2030@gmail.com>
2023-10-25 00:25:52 +0300

    intergaration with amazon lex

---
+++ shabir-dinner/components/chat.js
@@ -0,0 +1,28 @@
+import React, {useEffect} from 'react';
+// import Kommunicate from "@kommunicate/kommunicate-chatbot-plugin";
+
+const KommunicateChat = () => {
+  React.useEffect(() => {
+    (function(d, m) {
+      var kommunicateSettings = {
+        appId: '115d591cc261d5b808336b6a84cb51888',
+        popupWidget: true,
+        automaticChatOpenOnNavigation: true,
+      };
+      var s = document.createElement('script');
+      s.type = 'text/javascript';
+      s.async = true;
+      s.src = 'https://widget.kommunicate.io/v2/kommunicate.app';
+      var h = document.getElementsByTagName('head')[0];
+      h.appendChild(s);
+      window.kommunicate = m;
+      m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+  }, []);
+
+  return () => {
+    <div></div>;
+}
+};
+export default KommunicateChat;
+
--- shabir-dinner/src/pages/index.js
+++ shabir-dinner/src/pages/index.js
@@ -1,92 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Format from '../../layout/format';
 import Section1 from '../../components/section1';
 import Section2 from '../../components/section2';
 import Section3 from '../../components/section3';
 import Section4 from '../../components/section4';
 import Section5 from '../../components/section5';
-import { Box, Container, useColorMode } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import { IconButton } from '@chakra-ui/react';
 import { FaComments } from 'react-icons/fa';
 import { AiOutlineClose } from 'react-icons/ai';
-// import FaqSimple from '../../components/faq';
 import { MantineProvider } from '@mantine/core';
-import Adds from '../../components/adds';
-// import ContactForm from '../../components/contactForm';
+import KommunicateChat from '../../components/chat';
 
 export default function Home() {
-   const [isFormVisible, setIsFormVisible] = useState(false);
-  // const { colorMode } = useColorMode();
+  const [isFormVisible, setIsFormVisible] = useState(false);
 
   const toggleForm = () => {
     setIsFormVisible(!isFormVisible);
   };
+
   return (
     <MantineProvider>
-    <Format>
-      <Section1 />
-      <Section5 />
-      <Section3 />
-      <Section2 />
-      <Box
-          position='fixed'
-          bottom='4'
-          right='4'
-          zIndex='999'
-          display='flex'
-          alignItems='center'
-          justifyContent='center'
-          borderRadius='full'
-          style={{ backgroundColor: 'rgb(234, 179, 8)' }}
-        >
-          <IconButton
-          icon={<FaComments />}
-            color='white'
-            borderRadius='full'
-            boxShadow='lg'
-            cursor='pointer'
-            fontSize='32px'
-            width='52px'
-            height='50px'
-            _hover={{ bg: 'blue.600', transform: 'scale(1.05)' }}
-            aria-label='Open Chat'
-            onClick={toggleForm}
-          />
-        </Box>
-        {isFormVisible && (
-          <Box
-            position='fixed'
-            bottom='4'
-            right='4'
-            bg='gray.50'
-            borderRadius='lg'
-            boxShadow='lg'
-            p='4'
-            _dark={{ bg: 'gray.800' }}
-            zIndex='9999'
-            width={['100%', '90%', '80%']} // Responsive width
-            maxW='sm' // Limit max width on larger screens
-          >
-
-        {/*<ContactForm />*/}
-            <IconButton
-              icon={<AiOutlineClose />}
-              bg='blue.500'
-              color='white'
-              borderRadius='full'
-              fontSize='24px'
-              width='48px'
-              height='48px'
-              _hover={{ bg: 'blue.600', transform: 'scale(1.05)' }}
-              aria-label='Close Chat'
-              onClick={toggleForm}
-              mt='2'
-            />
-          </Box>
-        )}
-      {/*<FaqSimple />*/}
-      <Section4 />
-    </Format>
+      <Format>
+        <Section1 />
+        <Section5 />
+        <Section3 />
+        <Section2 />
+        <KommunicateChat />
+        <Section4 />
+      </Format>
     </MantineProvider>
-  )
+  );
 }
