e2e7dd7fdb36bf079f1fae02ffd7a1987b204929
erickadikah <erickadikah2030@gmail.com>
2023-10-25 01:52:34 +0300

    updated hero vedio

--- shabir-dinner/components/section1.js
+++ shabir-dinner/components/section1.js
@@ -1,53 +1,61 @@
 import React from "react";
-import Image from "next/image";
 import { motion } from "framer-motion";
+import { Button } from "@mui/material";
+import { Typography } from "@mui/material";
 
 const Section1 = () => {
-  const sectionStyle = {
-    backgroundImage: `linear-gradient(rgba(26, 25, 25, 0.8), rgba(21, 21, 21, 0.8)), url('/images/background.jpg')`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    minHeight: "92vh",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    padding: "2rem",
-    backgroundAttachment: "fixed",
-    backgroundRepeat: "no-repeat",
-    color: "#ffffff",
-    textAlign: "center",
-  };
-
-
   return (
-    <section style={sectionStyle}>
-      <motion.div
-        initial={{ opacity: 0, y: -20 }}
-  animate={{ opacity: 1, y: 0, transition: { duration: 1 } }}
-  transition={{
-    type: "spring",
-    stiffness: 260,
-    damping: 20,
-    delay: 0.5,
-  }} className="sm:mb-3 lg:mb-10">
-        <h1 className="sm:text-3xl lg:text-4xl font-extrabold text-[rgb(165,243,252)] text-800 lg:p-8">
-          Indulge your senses in a world of mouthwatering flavors, culinary inspiration,
-          <br />
-          and gastronomic adventures at Shabir Dinner.
-        </h1>
-        <p className="sm:text-lg lg:text-xl mb-4 lg:p-8">
-          At Shabir Dinner, we believe that good food is not only about taste but also about the stories behind it.
-          <br />
-          We are passionate about connecting people with the best local restaurants around them.
-        </p>
-      </motion.div>
-      <button className="hover:bg-yellow-600 text-white border border-yellow-500 font-bold py-2 px-4 rounded-sm w-60">
-        Order Now
-      </button>
+    <section className="bg-gradient relative min-h-screen flex items-center text-white">
+      <div className="absolute inset-0 z-[-1]">
+        <video autoPlay loop muted className="w-full h-full object-cover">
+          <source src="/vedio/background.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="absolute inset-0 bg-opacity-70 bg-black"></div>
+      </div>
+      <div className="container mx-auto text-center relative">
+        <motion.div
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+          transition={{
+            type: "spring",
+            stiffness: 260,
+            damping: 20,
+            delay: 0.5,
+          }}
+          className="mb-8"
+        >
+          <Typography
+            variant="h2"
+            component="h1"
+            className="text-cyan-300 font-extrabold mb-4"
+            sx={{
+              fontSize: {
+                xs: "0.7rem", // Small screens
+                sm: "1.5rem", // Medium screens
+                lg: "5rem", // Large screens
+              },
+            }}
+          >
+            Indulge your senses in a world of mouthwatering flavors, culinary
+            inspiration.
+          </Typography>
+          <Typography variant="body1" className="text-gray-300 mb-4">
+            At Shabir Dinner, we believe that good food is not only about taste
+            but also about the stories behind it.
+            <br />
+            Experience the magic of culinary inspiration in every dish we serve.
+          </Typography>
+        </motion.div>
+        <Button
+          variant="contained"
+          className="hover:bg-yellow-600 text-white font-semibold px-6 py-2 rounded-full"
+        >
+          Order Now
+        </Button>
+      </div>
     </section>
   );
 };
 
 export default Section1;
-
