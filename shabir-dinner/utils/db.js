ba4b2d7b8b5eb43d6f1f4e0b68b71b5a459c24bd
Erickadikah <erickadikah2030@gmail.com>
2023-12-16 20:17:59 +0300

    update storage && signup signin

--- shabir-dinner/utils/db.js
+++ shabir-dinner/utils/db.js
@@ -2,7 +2,6 @@
 import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI ;
-// 'mongodb+srv://erickadikah2030:7IxsoEWfMRZTrtGC@cluster0.po22n0u.mongodb.net/ArtGalary';
 
 const connectToDatabase = async () => {
   try {
